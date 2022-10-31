1e986d791e6f4455e432552531ac8fb06075ab54
Matheus Avelino Da Silva <matheusavav@gmail.com>
2022-10-31 10:18:24 -0300

    exercício_jquery

--- main.js
+++ main.js
@@ -2,10 +2,30 @@ $(document).ready(function() {
     $('form').on('submit', function(e) {
         e.preventDefault();
         const nomeTarefa = $('#nomeDaTarefa').val();
-        const novaTarefa = $('<li></li>');
-        $(`${nomeTarefa}`).appendTo(novaTarefa);
+        const novaTarefa = $(`<li>${nomeTarefa}</li>`)
         
         $(novaTarefa).appendTo('ul');
-        console.log;(nomeTarefa)
     })
+    /*$('.status').click(function() {
+        if ($(this).hasClass('ativar')) {
+            $(this).removeClass('ativar').addClass('desativar');
+        }
+        else {
+            $(this).removeClass('desativar').addClass('ativar');
+        }
+    });*/
 })
+
+$('ul').on("click", "li", function (e) {
+    e.preventDefault();
+    var ativo = $(this).addClass('activate');
+    /*$(this).html(ativo ? "Desativar" : "Ativar").toggleClass("activate deactivate");*/
+});
+
+/*$('ul').on("click", "li", function (e) {
+    e.preventDefault();
+    const subli = $(`<del></del>`)
+        
+    $(subli).appendTo('li');
+});*/
+
